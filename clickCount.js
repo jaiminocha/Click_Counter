c6f9437a96ca97d9a464aea890ab5a637383be09
Jai Minocha <edit>
2021-12-31 16:04:49 +0530

    Click Counter Commit

---
+++ clickCount.js
@@ -0,0 +1,9 @@
+"use strict"
+var box = document.getElementById('box');
+var clickCount = document.getElementById('clickCount');
+var count = 0;
+box.addEventListener('click', function() {
+    count++;
+    console.log(count);
+    clickCount.innerText = count + " ";
+});
